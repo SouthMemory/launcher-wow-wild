285b3970dbc1f4008cf31c5c1b97aede8677cd16
anyway <346454236@qq.com>
2024-07-05 21:59:39 +0800

    dd

--- install.py
+++ install.py
@@ -109,7 +109,7 @@ print(f"File list has been generated and written to {output_file}")
 
 # 创建 Launcher.zip
 create_zip_file(zip_file_path, executable_path)
-shutil.copy(executable_path, "DiDiLauncher.exe")
+shutil.copy(executable_path, "updates/DiDiLauncher.exe")
 
 # 读取版本号并写入到launcherversion.conf
 version = read_version_from_csproj(project_file_path)
